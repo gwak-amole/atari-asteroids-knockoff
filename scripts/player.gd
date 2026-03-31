85edfbaa598ae506efc907f6bb57e55662ba0bfd
gwak-amole <gwaksophie@gmail.com>
2026-03-31 11:08:01 -0700

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+@export var speed = 700.0;
+
+func _process(delta):
+	look_at(get_global_mouse_position())
+
+func _physics_process(delta):
+	var direction = (get_global_mouse_position() - global_position)
+	
+	if Input.is_action_pressed("ui_accept"): 
+		velocity = direction.normalized() * speed
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
